3eb70ac706c54c0022bc69d8d5072abc60f60448
brunowu <xinzhe.wu1990@gmail.com>
2017-09-12 16:16:19 +0800

    second commit

--- matCombine.c
+++ matCombine.c
@@ -3,29 +3,6 @@
 
 static char help[] = "Block matrix generator";
 
-/*
-PetscErrorCode read_multiple_matrix(Mat * A, PetscPrintf *num){
-	PetscErrorCode ierr;
-	PetscBool flaga;
-	PetscViewer fd;
-	PetscInt size,sizea;
-
-	ierr=PetscOptionsGetString(NULL,PETSC_NULL,"-mfile",filea,PETSC_MAX_PATH_LEN-1,&flaga);CHKERRQ(ierr);
-	if (!flaga) {
-		PetscPrintf(PETSC_COMM_WORLD,"ERRO : mfile is not properly set\n",filea);
-		return 0;
-	}
-	PetscPrintf(PETSC_COMM_WORLD,"Loading Matrix : %s\n",filea);
-	ierr=PetscViewerBinaryOpen(PETSC_COMM_WORLD,filea,FILE_MODE_READ,&fd);CHKERRQ(ierr);
-	ierr = MatCreate(PETSC_COMM_WORLD,A);CHKERRQ(ierr);
-	ierr=MatLoad(*A,fd);CHKERRQ(ierr);
-	ierr=PetscViewerDestroy(&fd);CHKERRQ(ierr);
-	ierr=MatGetSize(*A,&size,&sizea);CHKERRQ(ierr);
-	PetscPrintf(PETSC_COMM_WORLD,"Loaded Matrix of size : %d %d\n",size,sizea);
-
-	return 0;
-}
-*/
 
 PetscErrorCode matCombine(){
 
@@ -40,7 +17,7 @@ PetscErrorCode matCombine(){
 	MatInfo     	minfo,Ainfo;
 	PetscBool 		nbflg, mtflg;
 	PetscErrorCode 	ierr;
-	Mat 			A, M;
+	Mat 			A;
 	PetscViewer    		output_viewer;
 	char           		matrixOutputFile[PETSC_MAX_PATH_LEN];
 	const PetscInt    	*cols;
@@ -48,79 +25,84 @@ PetscErrorCode matCombine(){
 	PetscInt		*gcols;
     PetscInt 		idxcol;
 	char			type[PETSC_MAX_PATH_LEN];
-	char 			*filea[PETSC_MAX_PATH_LEN];
-	PetscInt        mat_num;
-	PetscBool 		mat_flg;
+	char			m1file[PETSC_MAX_PATH_LEN];
+	char			m2file[PETSC_MAX_PATH_LEN];
 
-
+	PetscBool 		mat1flg,mat2flg;
+	PetscViewer 	mat1fd,mat2fd;
+	PetscInt 		size,mat1sizea,mat1sizeb,mat2sizea,mat2sizeb;
+	Mat 			mat1, mat2;
+	PetscInt 		m, n;
+	PetscInt 		i, j, ind;
+ 
 	MPI_Comm_size(PETSC_COMM_WORLD,&size);
 
 	PetscPrintf(PETSC_COMM_WORLD, "The MPI world size is %d \n", size);
 
-	PetscOptionsGetStringArray(NULL,"-mat",filea,&mat_num,&mat_flg);
+	ierr=PetscOptionsGetString(NULL,PETSC_NULL,"-m1file",m1file,PETSC_MAX_PATH_LEN-1,&mat1flg);CHKERRQ(ierr);
+	if (!mat1flg) {
+		PetscPrintf(PETSC_COMM_WORLD,"ERROR : The first matrix mfile is not properly set\n",filea);
+		return 0;
+	}
 
-    MatGetSize(M,&m, &n);
-    MatGetInfo(M,MAT_GLOBAL_SUM,&minfo);
-	nnz = minfo.nz_used;
-    PetscPrintf(PETSC_COMM_WORLD,"Already read matrix with properties : m = %d, n = %d, nnz = %g, type = real\n",m,n,nnz);
-    gm = nb * m;
-    gn = nb * n;
-    PetscPrintf(PETSC_COMM_WORLD,"\n]>> Start the generation of matrix : gm = %d, gn = %d, type = real\n",gm,gn);
+	PetscPrintf(PETSC_COMM_WORLD,"Loading First Matrix : %s\n",m1file);
+	ierr=PetscViewerBinaryOpen(PETSC_COMM_WORLD,m1file,FILE_MODE_READ,&m1fd);CHKERRQ(ierr);
+	ierr = MatCreate(PETSC_COMM_WORLD,&mat1);CHKERRQ(ierr);
+	ierr=MatLoad(mat1,m1fd);CHKERRQ(ierr);
+	ierr=PetscViewerDestroy(&m1fd);CHKERRQ(ierr);
+	ierr=MatGetSize(mat1,&mat1sizea,&mat1sizeb);CHKERRQ(ierr);
+	PetscPrintf(PETSC_COMM_WORLD,"Loaded First Matrix of size : %d %d\n",mat1sizea,mat1sizeb);
+
+	ierr=PetscOptionsGetString(NULL,PETSC_NULL,"-m2file",m2file,PETSC_MAX_PATH_LEN-1,&mat2flg);CHKERRQ(ierr);
+	if (!mat2flg) {
+		PetscPrintf(PETSC_COMM_WORLD,"ERROR : The Second matrix mfile is not properly set\n",filea);
+		return 0;
+	}
+
+	PetscPrintf(PETSC_COMM_WORLD,"Loading Second Matrix : %s\n",m2file);
+	ierr=PetscViewerBinaryOpen(PETSC_COMM_WORLD,m2file,FILE_MODE_READ,&m2fd);CHKERRQ(ierr);
+	ierr = MatCreate(PETSC_COMM_WORLD,&mat2);CHKERRQ(ierr);
+	ierr=MatLoad(mat2,m2fd);CHKERRQ(ierr);
+	ierr=PetscViewerDestroy(&m2fd);CHKERRQ(ierr);
+	ierr=MatGetSize(mat2,&mat2sizea,&mat2sizeb);CHKERRQ(ierr);
+	PetscPrintf(PETSC_COMM_WORLD,"Loaded Second Matrix of size : %d %d\n",mat1sizea,mat1sizeb);
+
+	m = mat1sizea + mat2sizea;
+	n = mat1sizeb + mat2sizea;
+
+    PetscPrintf(PETSC_COMM_WORLD,"\n]>> Start the generation of matrix : m = %d, n = %d, type = real\n",m,n);
 
 	ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
-  	ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,gm,gn);CHKERRQ(ierr);
+  	ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,m,n);CHKERRQ(ierr);
   	ierr = MatSetFromOptions(A);CHKERRQ(ierr);
   	ierr = MatSetUp(A);CHKERRQ(ierr);	
 	ierr = MatGetOwnershipRange(A, &start, &end);CHKERRQ(ierr);
-	ierr = MatGetOwnershipRange(M, &rstart, &rend);CHKERRQ(ierr);
 
-	if(strcmp(type, "matline") == 0){
-		for(k = 0; k< nb; k++){
-    			for(i = rstart; i < rend; i++){
-    				MatGetRow(M,i,&ncols,&cols,&vals);
-        			PetscMalloc1(ncols,&gcols);
-				idxcol = i + k*m;
-					for(k1 = 0; k1 < ncols; k1++){
-						gcols[k1] = cols[k1] + k*m;
-					}
-				ierr = MatSetValues(A,1,&idxcol,ncols,gcols,vals,INSERT_VALUES);CHKERRQ(ierr);
-    				MatRestoreRow(M,i,&ncols,&cols,&vals);
-	    		}
+	for(i = start; i < end; i++){
+		if(i < mat1sizea){
+			MatGetRow(mat1,i,&ncols,&cols,&vals);
+			PetscMalloc1(ncols,&gcols);
+			ierr = MatSetValues(A,1,&i,ncols,gcols,vals,INSERT_VALUES);CHKERRQ(ierr);
+			MatRestoreRow(M,i,&ncols,&cols,&vals);
+
 		}
-		for(k2 = start; k2 < end; k2++){
-			if (k2 % 100 == 0 && k2 < gm - 350){
-				gcol = k;
-                        	grow = k + 350;
-                        	PetscRandomGetValue(rnd,&rnd_value);
-                        	value=0.01*rnd_value;
-                    		ierr = MatSetValues(A,1,&grow,1,&gcol,&value,INSERT_VALUES);CHKERRQ(ierr);
-			}
+		else{
+			ind = i - mat1sizea;
+			MatGetRow(mat2,ind,&ncols,&cols,&vals);
+			PetscMalloc1(ncols,&gcols);
+			ierr = MatSetValues(A,1,&i,ncols,gcols,vals,INSERT_VALUES);CHKERRQ(ierr);	
+			MatRestoreRow(M,ind,&ncols,&cols,&vals);
 		}
 	}
 
-        if(strcmp(type, "matblock") == 0){
-                for(k = 0; k< nb; k++){
-                        for(i = rstart; i < rend; i++){
-                                MatGetRow(M,i,&ncols,&cols,&vals);
-                                PetscMalloc1(ncols,&gcols);
-                                idxcol = i + k*m;
-                                        for(k1 = 0; k1 < ncols; k1++){
-                                                gcols[k1] = cols[k1] + k*m;
-                                        }
-                                ierr = MatSetValues(A,1,&idxcol,ncols,gcols,vals,INSERT_VALUES);CHKERRQ(ierr);
-                                ierr = MatSetValues(A,1,&i,ncols,gcols,vals,INSERT_VALUES);CHKERRQ(ierr);
-			        MatRestoreRow(M,i,&ncols,&cols,&vals);
-                        }
-                }
-        }
 	/*Matrix assembly*/
 	PetscPrintf(PETSC_COMM_WORLD,"Assembling matrix within PETSc.\n");
 	MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);
 	MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);
 //	MatView(A,PETSC_VIEWER_STDOUT_WORLD);
-        MatGetSize(A, &ggm, &ggn);
-        MatGetInfo(A,MAT_GLOBAL_SUM,&Ainfo);
-        gnnz = Ainfo.nz_used;
+    MatGetSize(A, &ggm, &ggn);
+    MatGetInfo(A,MAT_GLOBAL_SUM,&Ainfo);
+    gnnz = Ainfo.nz_used;
 	PetscPrintf(PETSC_COMM_WORLD,"Finished matrix assembly.\n");
 	sprintf(matrixOutputFile,"%s_nb_%d_%dx%d_%g_nnz.gz",type, nb, ggm,ggn,gnnz);
 	PetscPrintf(PETSC_COMM_WORLD,"Dumping matrix to PETSc binary %s\n",matrixOutputFile);	
